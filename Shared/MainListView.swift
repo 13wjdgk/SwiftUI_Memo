95979317ca37e503e5883936f8df3b62845a2f41
김가은 <gimga-eun@king-gabug-i.local>
2022-07-31 19:04:54 +0900

    View change & Create Memo

--- Shared/MainListView.swift
+++ Shared/MainListView.swift
@@ -9,15 +9,30 @@ import SwiftUI
 
 struct MainListView: View {
     @EnvironmentObject var store : MemoStore //뷰가 생성되는 시점에 공유데이터를 확인하고, store 타입과 동일한 타입을 가진 인스턴스가 있다면 속성을 해당 인스턴스로 자동 초기화한다. 하나의 데이터를 여러 뷰에 이용할 때 자주 사용
+    
+    @State private var showComposer : Bool = false
+    
+    
     var body: some View {
         NavigationView {
             List(store.list){ memo in
                 Memocell(memo : memo)
                 
             }
+            .listStyle(.plain)
             .navigationTitle("내 메모")
+            .toolbar {
+                Button {
+                    showComposer = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showComposer){
+                ComposeView()
+            }
         }
-        .listStyle(.plain)
+        
     }
 }
 
